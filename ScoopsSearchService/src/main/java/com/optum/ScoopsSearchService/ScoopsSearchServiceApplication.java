919a7032bf611fe9a3b7b481698b7f359dd8094e
shashibongit <140369763+shashibongit@users.noreply.github.com>
2023-07-24 15:20:18 +0530

    Add files via upload
    Initial commit

---
+++ ScoopsSearchService/src/main/java/com/optum/ScoopsSearchService/ScoopsSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optum.ScoopsSearchService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoopsSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoopsSearchServiceApplication.class, args);
+	}
+
+}
---
+++ ScoopsSearchService/src/test/java/com/optum/ScoopsSearchService/ScoopsSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.ScoopsSearchService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoopsSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
